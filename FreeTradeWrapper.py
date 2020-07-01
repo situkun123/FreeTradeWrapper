63304c1f7f2111072efb64377030e30aa135f202
Kun <kun.situ@hotmail.com>
2020-07-01 16:50:20 +0100

    All functions can be used both symbol and isin.
    Now it requires gspread package and google sheet API

--- FreeTradeWrapper.py
+++ FreeTradeWrapper.py
@@ -1,12 +1,37 @@
+import gspread
+import pandas as pd
 import requests
 
 class FreeTrade(object):
-
-    def __init__(self, API_key):
+    # class method variables/properties
+    def __init__(self, API_key, gsheet_json):
         '''Ensure the json file is in local directory'''
+        self.con = gspread.service_account(gsheet_json)
+        self.lookup_df = self.all_symbol_info()
         self.API_key = API_key
 
-        def get_symbol(self, isin):
+    def _check_sym_isin(self, x):
+        '''Identify if the input is a symbol or isin'''
+        if len(x) < 6: # symbol
+            return self.find_isin(x)
+        else:
+            return x
+
+    def all_symbol_info(self):
+        data = self.con.open_by_key('14Ep-CmoqWxrMU8HshxthRcdRW8IsXvh3n2-ZHVCzqzQ').get_worksheet(0).get_all_values()
+        data_df = pd.DataFrame(data[1:], columns=data[0])
+        return data_df
+
+    def find_isin(self, symbol):
+        '''Using a symbol to find isin number'''
+        df = self.lookup_df
+        output = df[df['symbol']== symbol]['isin'].values[0]
+        if len(output) == 0:
+            return 'No Symbol found!'
+        else:
+            return output
+
+    def get_symbol(self, isin):
         '''get symbol of a stock'''
         self.url_symbol = f'https://finki.io/callAPI.php?isin={isin}&function=ukSymbol&key={self.API_key}'
         con = requests.get(self.url_symbol)
@@ -16,9 +41,10 @@ class FreeTrade(object):
             symobol = con.content.decode("utf-8").strip('\n')
             return symobol
 
-    def get_bid_price(self, isin):
+    def get_bid_price(self, input1):
         '''Get the bid price of a company'''
-        self.url_bid = f'https://finki.io/callAPI.php?isin={isin}&function=ukBid&key={self.API_key}'
+        input1 = self._check_sym_isin(input1)
+        self.url_bid = f'https://finki.io/callAPI.php?isin={input1}&function=ukBid&key={self.API_key}'
         con = requests.get(self.url_bid)
         if con.status_code != 200:
             return 'Connection failed!'
@@ -26,9 +52,10 @@ class FreeTrade(object):
             bid = float(con.content)
             return bid
 
-    def get_ask_price(self, isin):
+    def get_ask_price(self, input1):
         '''Get the bid price of a company'''
-        self.url_ask = f'https://finki.io/callAPI.php?isin={isin}&function=ukAsk&key={self.API_key}'
+        input1 = self._check_sym_isin(input1)
+        self.url_ask = f'https://finki.io/callAPI.php?isin={input1}&function=ukAsk&key={self.API_key}'
         con = requests.get(self.url_ask)
         if con.status_code != 200:
             return 'Connection failed!'
@@ -37,9 +64,10 @@ class FreeTrade(object):
             return bid
 
     
-    def get_currency(self, isin):
+    def get_currency(self, input1):
         '''Get the currency price of a company'''
-        self.url_currency = f'https://finki.io/callAPI.php?isin={isin}&function=ukCurrency&key={self.API_key}'
+        input1 = self._check_sym_isin(input1)
+        self.url_currency = f'https://finki.io/callAPI.php?isin={input1}&function=ukCurrency&key={self.API_key}'
         con = requests.get(self.url_currency)
         if con.status_code != 200:
             return 'Connection failed!'
@@ -48,9 +76,10 @@ class FreeTrade(object):
             return currency
 
     
-    def get_dividendData(self, isin):
+    def get_dividendData(self, input1):
         '''Get dividend data'''
-        self.url_dividend = f'https://finki.io/callAPI.php?isin={isin}&function=dividendData&key={self.API_key}'
+        input1 = self._check_sym_isin(input1)
+        self.url_dividend = f'https://finki.io/callAPI.php?isin={input1}&function=dividendData&key={self.API_key}'
         con = requests.get(self.url_dividend)
         if con.status_code != 200:
             return 'Connection failed!'
@@ -58,12 +87,13 @@ class FreeTrade(object):
             dividend = con.content.decode("utf-8").strip('\n')
             return dividend
 
-
-    def get_all_info(self, isin):
+    
+    def get_all_info(self, input1):
         '''Get bid, ask, and currency information'''
-        bid = self.get_bid_price(isin)
-        ask = self.get_ask_price(isin)
-        currency = self.get_currency(isin)
-        symbol = self.get_symbol(isin)
+        input1 = self._check_sym_isin(input1)
+        bid = self.get_bid_price(input1)
+        ask = self.get_ask_price(input1)
+        currency = self.get_currency(input1)
+        symbol = self.get_symbol(input1)
 
-        return {'isin': isin, 'Symbol': symbol, 'bid':bid, 'ask':ask, 'currency':currency}
+        return {'isin': input1, 'Symbol': symbol, 'bid':bid, 'ask':ask, 'currency':currency}
